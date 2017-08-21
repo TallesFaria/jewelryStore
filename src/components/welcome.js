99e4f1398a9f1fdbd035ce950f2f3f110028e00a
TallesFaria <tallesbeginsc@gmail.com>
2017-08-21 11:53:56 -0300

    welcome display personalized info

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import * as actions from '../actions';
 import AgendaDia from './abas/Client/AgendaDia';
 
-class Welcome extends Component {    
-
-    render() {
-        return (
+class Welcome extends Component {
+    renderLinks() {
+        if (this.props.authenticated) {
+            return (
+                <AgendaDia />
+            );
+        }
+        return [
             <div>
                 <div className="pagina-inicial">
                     <span>Por favor, faça login!</span>
                 </div>
             </div>
+        ];
+    }
+    
+    render() {
+        return (
+            <nav className="navs">
+                <Link to="/inicio" className="navbar-brand"><h2>Início</h2></Link>
+                <ul>
+                    {this.renderLinks()}
+                </ul>
+            </nav>
         );
     }
 }
 
-export default (Welcome);
+const mapStateToProps = (state) => {
+    return {
+        authenticated: state.auth.authenticated
+    };
+};
+
+export default connect(mapStateToProps, actions)(Welcome);
